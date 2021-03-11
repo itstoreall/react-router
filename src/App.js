8d4201444feb86c46d1cdc5aaedd2a5aad9f0b35
Serhii Stanislav <serhiistanislav@gmail.com>
2021-03-11 21:48:27 +0200

    adds Route, Link, Switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import AuthorsView from "./views/AuthorsView";
+import BooksView from "./views/BooksView";
+import NotFoundView from "./views/NotFoundView";
 
-function App() {
-  return <p>Hi from App!</p>;
-}
+const App = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/authors">Authors</Link>
+        </li>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/" component={HomeView} />
+        <Route path="/authors" component={AuthorsView} />
+        <Route path="/books" component={BooksView} />
+        <Route component={NotFoundView} />
+      </Switch>
+    </>
+  );
+};
 
 export default App;
---
+++ src/views/BooksView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BooksView = () => {
+  return <h1>Books Page</h1>;
+};
+
+export default BooksView;
